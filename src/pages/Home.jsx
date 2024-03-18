5091fcfdede3a24a2e01ba51646dc9a055115d82
Eyad El Khodary <eyad@Eyads-MacBook-Air.local>
2024-03-18 15:02:42 -0400

    added home page w/ background img and text

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="overlay"></div>
+      <div className="content">
+        <p className="title">QUEENS CRICKET CLUB</p>
+        <p className="intro">Introducing Cricket to Queen's University and Kingston Community</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
